27229a0cf3b12a095bbcca953cb7fdb7caa6414e
nam <namhoangpham96@gmail.com>
2022-10-07 13:13:23 -0400

    Current front end showcase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './App.css';
 import { NavBar, List } from './components';
-import { Header, Footer } from './containers'
+import { Header, Footer } from './containers';
 
 const App = () => {
   return (
