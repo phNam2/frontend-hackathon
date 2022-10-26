539f2ae960107ad7b5ac5fc07473624491846737
nam <namhoangpham96@gmail.com>
2022-10-26 16:45:04 -0400

    Current pages showcase

--- src/pages/advocateList/AdvocatesList.jsx
+++ src/pages/advocateList/AdvocatesList.jsx
@@ -3,29 +3,40 @@ import React, { useState, useEffect } from 'react'
 const AdvocatesList = () => {
 
     let [advocates, setAdvocates] = useState([])
+    let [pages, setPagesNumber] = useState([])
+    let [pagesInfo, setPagesInfo] = useState([])
     useEffect(() => {
         getAdvocates()
     }, [])
 
     let getAdvocates = async() => {
-        let response = await fetch('https://cados.up.railway.app/advocates/')
+        let response = await fetch('https://cados.up.railway.app/advocates/?page=3')
         let data = await response.json()
-        console.log('Data:', data.advocates)
-        setAdvocates(data.advocates)
+        console.log(data.pagination.pages)
+        setAdvocates(data.advocates) // Get the advocates information from API
+        setPagesInfo(data.pagination)
+        setPagesNumber(data.pagination.pages) // Get the current pagination information from API
     }
 
     return (
         <div>
             <div>AdvocatesList:</div>
+            {/* Advocate information */}
             <div className="advocates-list">
                 {advocates.map((advocate, i) =>(
                     <h3 key={i}>{advocate.name}</h3>
                 ))}
             </div>
-        </div>
-        
 
-        
+            {/* Pages number showcase */}
+            <div className="pages-number">
+                {pages.map((page, i) =>(
+                    <li key={i}>{page}</li>
+                ))}
+            </div>
+            
+            <h3>{pagesInfo.total_pages}</h3>
+        </div>      
     )
 }
 
