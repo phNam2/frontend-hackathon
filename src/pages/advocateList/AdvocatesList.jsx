3a91a9bc9f194159c4dd2d63f6b2f7bd85f48772
nam <namhoangpham96@gmail.com>
2022-10-27 16:51:00 -0400

    Current design for button page

--- src/pages/advocateList/AdvocatesList.jsx
+++ src/pages/advocateList/AdvocatesList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './AdvocateList.css'
 
 const AdvocatesList = () => {
 
@@ -6,16 +7,21 @@ const AdvocatesList = () => {
     let [pages, setPagesNumber] = useState([])
     let [pagesInfo, setPagesInfo] = useState([])
     useEffect(() => {
-        getAdvocates()
+        getAdvocates(50)
     }, [])
 
-    let getAdvocates = async() => {
-        let response = await fetch('https://cados.up.railway.app/advocates/?page=3')
+    let getAdvocates = async(current_page) => {
+        let response = await fetch('https://cados.up.railway.app/advocates/?page='+ current_page)
         let data = await response.json()
         console.log(data.pagination.pages)
         setAdvocates(data.advocates) // Get the advocates information from API
         setPagesInfo(data.pagination)
         setPagesNumber(data.pagination.pages) // Get the current pagination information from API
+    }
+
+
+    const goToPage = (event) => {
+        getAdvocates(Number(event.target.id))
     }
 
     return (
@@ -29,13 +35,13 @@ const AdvocatesList = () => {
             </div>
 
             {/* Pages number showcase */}
-            <div className="pages-number">
+            <ul className="pages-number">
                 {pages.map((page, i) =>(
-                    <li key={i}>{page}</li>
+                    <li key={i} id={page} class="pages-button" onClick={goToPage}>{page}</li>
                 ))}
-            </div>
+            </ul>
             
-            <h3>{pagesInfo.total_pages}</h3>
+            <h3>{pagesInfo.current_page}</h3>
         </div>      
     )
 }
