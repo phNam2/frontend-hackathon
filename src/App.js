8dfef8504c09ed67809512fc34ddc18f0b59ba45
nam <namhoangpham96@gmail.com>
2022-11-02 17:09:13 -0400

    Add the follow list

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 import './index.css';
 import { NavBar } from './components';
 import { Header, Footer } from './containers';
-import { AdvocateList, AdvocateDetails } from './pages';
+import { AdvocateList, AdvocateDetails, FollowList } from './pages';
 
 const App = () => {
   return (
@@ -20,6 +20,7 @@ const App = () => {
           <Header/>
         </div>
         <Routes>
+          <Route path="/follow" element={<FollowList /> } />
           <Route path="/advocates" element={<AdvocateList /> } />
           <Route path="/advocates/:username" element={<AdvocateDetails />}/>
         </Routes>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export {default as AdvocateList} from './advocateList/AdvocatesList'
 export {default as AdvocateDetails} from './advocateDetails/AdvocateDetails'
+export {default as FollowList} from './followList/FollowList'
