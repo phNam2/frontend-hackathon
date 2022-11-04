92e7f3d9bb0ab438f2ab53316babb9e822a8a037
nam <namhoangpham96@gmail.com>
2022-11-04 13:47:49 -0400

    Current design for advocate card

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
 
 const ListItem = ({advocates}) => {
-  // console.log(advocates);
+  
+  const navigate = useNavigate();
+  const goToAdvocatePage = (username) => {
+    navigate('/advocates/'+username );
+}
 
   return (
-    <div>
-      <div className="advocates-list">
+    <div className='"advocates_list"'>
+      <div className="advocates_list-container">
         {advocates.map((advocate, i) =>(
-          <div>
-            <a href={'/advocates/'+advocate.username} key={i}>{advocate.name}</a>
+          <div class="details" onClick={() => goToAdvocatePage(advocate.username)}>
+            <img src={advocate.profile_pic} />
+            <a href={'@'+advocate.twitter} key={i}>{advocate.username}</a>
+            {/* <a href={'/advocates/'+advocate.username} key={i}>{advocate.name}</a> */}
+            <div className="more">
+              <h1>{advocate.name}</h1>
+              <p>{advocate.bio}</p>
+            </div>
+
           </div>
           
         ))}
