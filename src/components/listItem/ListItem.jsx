6f62cbaf032a023c1f37a0bab1f7686c422d1914
nam <namhoangpham96@gmail.com>
2022-11-04 15:21:28 -0400

    Current design

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom';
+import {RiUserFollowFill} from 'react-icons/ri'
 import './listitem.css'
 
 const ListItem = ({advocates}) => {
@@ -13,12 +14,16 @@ const ListItem = ({advocates}) => {
     <div className='"advocates_list"'>
       <div className="advocates_list-container">
         {advocates.map((advocate, i) =>(
-          <div className="details" onClick={() => goToAdvocatePage(advocate.username)}>
+          <div className="details">
             <img src={advocate.profile_pic} />
-            <p>{'@'+advocate.username}</p>
+            <a href={advocate.twitter}>{'@'+advocate.username}</a>
             <div className="details-more">
               <h1>{advocate.name}</h1>
               <p>{advocate.bio}</p>
+            </div>
+            <div className="details-buttons">
+              <RiUserFollowFill size={27} onClick={null} />
+              <button onClick={() => goToAdvocatePage(advocate.username)}>Details</button>
             </div>
 
           </div>
